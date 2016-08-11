fa9dc9eb9d352138725fd849c91da95deb3fa0c4
Ian Milliken <ian@existent.co.uk>
2016-08-11 16:13:59 +0100

    uncommented that stuff

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -16,11 +16,11 @@ CarrierWave.configure do |config|
     config.aws_bucket       = ENV['S3_BUCKET']
     config.aws_acl          = 'public-read'
 
-    #config.aws_credentials  = {
-    #  access_key_id:      ENV['S3_KEY_ID'],
-    #  secret_access_key:  ENV['S3_SECRET_KEY'],
-    #  region:             ENV['S3_BUCKET_REGION']
-    #}
+    config.aws_credentials  = {
+      access_key_id:      ENV['S3_KEY_ID'],
+      secret_access_key:  ENV['S3_SECRET_KEY'],
+      region:             ENV['S3_BUCKET_REGION']
+    }
 
     # Put your CDN host below instead
     if ENV['S3_ASSET_HOST_URL'].present?
